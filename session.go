d1b094e9fcfdb80acac628328e4db12fa799da3d
Keith Rarick <kr@xph.us>
2015-01-15 12:10:37 -0800

    switch to secretbox

--- session.go
+++ session.go
@@ -1,12 +1,14 @@
 package session
 
 import (
+	"crypto/rand"
+	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"net/http"
 	"time"
 
-	"github.com/fernet/fernet-go"
+	"golang.org/x/crypto/nacl/secretbox"
 )
 
 const (
@@ -22,13 +24,14 @@ type Config struct {
 
 	// Maximum idle time for a session.
 	// This is used to set cookie expiration and
-	// enforce TTL on fernet tokens.
+	// enforce a TTL on secret boxes.
 	// If 0, it is taken to be 100 years.
 	MaxAge time.Duration
 
-	// List of acceptable keys for decoding stored sessions.
+	// List of acceptable secretbox keys for decoding stored sessions.
 	// Element 0 will be used for encoding.
-	Keys []*fernet.Key
+	// See golang.org/x/crypto/nacl/secretbox.
+	Keys []*[32]byte
 }
 
 func (c *Config) maxAge() time.Duration {
@@ -60,28 +63,45 @@ func Get(r *http.Request, v interface{}, config *Config) error {
 		return err
 	}
 	t := []byte(cookie.Value)
-	b := fernet.VerifyAndDecrypt(t, config.maxAge(), config.Keys)
-	if b == nil {
-		return ErrInvalid
+	var nonce [24]byte
+	copy(nonce[:], t)
+	out := make([]byte, len(t)-secretbox.Overhead-24)
+	for _, key := range config.Keys {
+		if tb, ok := secretbox.Open(out, t[24:], &nonce, key); ok {
+			ts := binary.BigEndian.Uint64(tb)
+			if time.Since(time.Unix(int64(ts), 0)) > config.maxAge() {
+				return ErrInvalid
+			}
+			b := tb[8:]
+			return json.Unmarshal(b, v)
+		}
 	}
-	return json.Unmarshal(b, v)
+	return ErrInvalid
 }
 
 // Set encodes a session from v into a cookie on w.
 // See encoding/json for encoding behavior.
 func Set(w http.ResponseWriter, v interface{}, config *Config) error {
+	now := time.Now()
 	b, err := json.Marshal(v)
 	if err != nil {
 		return err
 	}
-	t, err := fernet.EncryptAndSign(b, config.Keys[0])
+	tb := make([]byte, len(b)+8)
+	binary.BigEndian.PutUint64(tb, uint64(now.Unix()))
+	copy(tb[8:], b)
+	out := make([]byte, 24+len(tb)+secretbox.Overhead)
+	var nonce [24]byte
+	_, err = rand.Read(nonce[:])
 	if err != nil {
 		return err
 	}
+	copy(out, nonce[:])
+	secretbox.Seal(out[24:], tb, &nonce, config.Keys[0])
 	cookie := config.Cookie
 	cookie.Name = config.name()
-	cookie.Value = string(t)
-	cookie.Expires = time.Now().Add(config.maxAge())
+	cookie.Value = string(out)
+	cookie.Expires = now.Add(config.maxAge())
 	if cookie.Path == "" {
 		cookie.Path = "/"
 	}
